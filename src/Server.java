66b674deece3d4522794961f5d1fc0a18b09257a
sarnobat <ss401533@gmail.com>
2013-08-23 20:36:29 -0700

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.sun.net.httpserver.HttpServer;
+
+public class Server {
+	@Path("helloworld")
+	public static class HelloWorldResource { // Must be public
+
+		@GET
+		@Path("json")
+		@Produces("application/json")
+		public String json() throws JSONException {
+			JSONObject json = new JSONObject();
+			json.put("foo", "bar");
+			return json.toString();
+		}
+	}
+
+	public static void main(String[] args) throws URISyntaxException {
+		HttpServer server = JdkHttpServerFactory.createHttpServer(
+				new URI("http://localhost:9099/"), new ResourceConfig(HelloWorldResource.class));
+	}
+}
